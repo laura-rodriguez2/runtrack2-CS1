1545a24c237c762d7c4653ee60fd5b6f4f25cfff
laura-rodriguez2 <laura.rodriguez@laplateforme.io>
2021-11-09 11:17:59 +0100

    j7

---
+++ jour07/job01/index.php
@@ -0,0 +1,16 @@
+<?php
+session_start();
+if(isset($_SESSION['nbvisite'])){
+    $_SESSION['nbvisite']++;
+
+}
+if(isset($_POST['reset'])){
+    $_SESSION['nbvisite']=0;
+}
+echo($_SESSION['nbvisite'];
+
+?>
+
+<form method="post" action="index.php">
+    <input type="submit" name="reset" class="button" value="reset" >
+</form>
